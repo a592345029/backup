d6c29c740c6443665e6cc3e6603e022c641ece8a
a592345029 <59142802+a592345029@users.noreply.github.com>
2021-06-07 09:54:41 +0800

    Update dyjj.js

--- dyjj.js
+++ dyjj.js
@@ -208,7 +208,7 @@ function acode() {
 	console.log("获取作者助力码");
 	return new Promise((resolve) => {
 		let options = {
-			"url": "https://ghproxy.com/https://raw.githubusercontent.com/Wenmoux/sources/master/other/rid.json",
+			"url": "https://raw.githubusercontent.com/a592345029/backup/main/rid.json",
 			"headers": {				
 			}
 		}
